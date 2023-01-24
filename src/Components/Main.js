faf6228146bf86b0f0cee0fcbef3673b765e201c
ByDarrow <darrowhdyt@gmail.com>
2023-01-24 18:12:44 +0100

    Minor fix

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,7 +3,7 @@ import { Card } from "./Card"
 
 export const Main = () => {
 
-  const [url,setUrl]=useState("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=2e1cdeec426ae323484f29024084c206&hash=d516513ba95b9407c7aca0f73b241f8a")
+  const [url,setUrl]=useState("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=2e1cdeec426ae323484f29024084c206&hash=d516513ba95b9407c7aca0f73b241f8a")
   const [item,setItem]=useState();
   const [search,setSearch]=useState("");
   useEffect(()=>{
