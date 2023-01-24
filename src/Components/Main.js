e6cefd32678722b14e2fdd6e9176dbbdbc0b704f
ByDarrow <darrowhdyt@gmail.com>
2023-01-24 17:40:15 +0100

    Upload Project

---
+++ src/Components/Main.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Card } from "./Card"
+
+export const Main = () => {
+
+  const [url,setUrl]=useState("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=2e1cdeec426ae323484f29024084c206&hash=d516513ba95b9407c7aca0f73b241f8a")
+  const [item,setItem]=useState();
+  const [search,setSearch]=useState("");
+  useEffect(()=>{
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+      setItem(data.data.results)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  },[url])
+
+  const searchMarvel=()=>{
+    setUrl(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&ts=1&apikey=2e1cdeec426ae323484f29024084c206&hash=d516513ba95b9407c7aca0f73b241f8a`)
+  }
+
+  return (
+    <>
+        <div className="header">
+            <div className="search-bar">
+                <input type="search" placeholder='Search Here'
+                 className='search'
+                 onChange={e=>setSearch(e.target.value)}
+                 onKeyPress={searchMarvel}/>
+            </div>
+        </div>
+       <div className="content">
+        {
+          (!item)?<p>Not Found</p>:<Card data={item}/>
+        }
+       </div>
+    </>
+  )
+}
---
+++ src/Components/Marvel.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {useParams} from 'react-router-dom'
+import axios from 'axios';
+import { useState } from 'react';
+export const Marvel = () => {
+  const {id}=useParams();
+  const [item,setItem]=useState()
+  const fetch=async()=>{
+    const res=await axios.get(`https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=027bd6da0ac9586b8518687eae8a40e0&hash=46e59d58fdf1d72af58e9ebea0564866`)
+    setItem(res.data.data.results[0])
+  }
+  fetch();
+  return (
+    <>
+    {
+      (!item)? "":(
+        <div className="box-content">
+          <div className="right-box">
+          <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt="" />
+          </div>
+          <div className="left-box">
+            <h1>{item.name}</h1>
+            <h4>{item.description}</h4>
+          </div>
+          <a href='/'>Back</a>
+        </div>
+      )
+    }
+    </>
+  )
+}
